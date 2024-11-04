8249fd336cd8c0e7a03c942ea22b43f684da5354
dreadedhamish <hamish.erskine@gmail.com>
2024-11-04 14:21:52 +1100

    ..

--- setup-fontconfig.sh
+++ setup-fontconfig.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
-# Find the path to the fontconfig binaries
-FONTCONFIG_BIN=$(find /nix/store -name fc-cache | head -n 1 | xargs dirname)
+# # Find the path to the fontconfig binaries
+# FONTCONFIG_BIN=$(find /nix/store -name fc-cache | head -n 1 | xargs dirname)
 
-# Ensure fontconfig binaries are in the PATH
-export PATH=$PATH:$FONTCONFIG_BIN
+# # Ensure fontconfig binaries are in the PATH
+# export PATH=$PATH:$FONTCONFIG_BIN
 
-mkdir -p /etc/fonts/conf.d
-cat <<EOT > /etc/fonts/fonts.conf
-<?xml version="1.0"?>
-<!DOCTYPE fontconfig SYSTEM "fonts.dtd">
-<fontconfig>
-  <dir>/nix/store/*-noto-fonts-*/share/fonts</dir>
-  <cachedir>/var/cache/fontconfig</cachedir>
-  <configdir>/etc/fonts/conf.d</configdir>
-</fontconfig>
-EOT
+# mkdir -p /etc/fonts/conf.d
+# cat <<EOT > /etc/fonts/fonts.conf
+# <?xml version="1.0"?>
+# <!DOCTYPE fontconfig SYSTEM "fonts.dtd">
+# <fontconfig>
+#   <dir>/nix/store/*-noto-fonts-*/share/fonts</dir>
+#   <cachedir>/var/cache/fontconfig</cachedir>
+#   <configdir>/etc/fonts/conf.d</configdir>
+# </fontconfig>
+# EOT
 
 # Update the font cache
 fc-cache -f -v
